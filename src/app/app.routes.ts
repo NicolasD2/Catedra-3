d7d69fcecba25ec75380541aeba8e56f69b9d593
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-01-10 16:39:06 -0300

    feat: add register and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './AuthManagement/components/login/login.component';
+import { RegisterComponent } from './AuthManagement/components/register/register.component';
+import { authGuard } from './AuthManagement/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login',component: LoginComponent }, // Página de registro
+  // pa ver los post {path: 'clients',component: ClientmanagementComponent,canActivate: [authGuard],},
+  {path: 'register',component: RegisterComponent},
+
+];
